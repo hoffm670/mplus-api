67d0caf6c90b2b1baf58bf7daf5197c1042ee462
Brandon Hoffmann <hoffm670@gmail.com>
2024-03-11 00:50:38 -0500

    added support for all regions

--- app/constants.py
+++ app/constants.py
@@ -1,6 +1,3 @@
-# Regions
-REGION_US = 'us'
-
 # Affixes
 FORT = 'Fortified'
 FORT_ID = 10
---
+++ app/models/regions.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Region(Enum):
+    US = 'us'
+    EU = 'eu'
+    TW = 'tw'
+    KR = 'kr'
