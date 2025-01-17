e37211e1e5898e87e3d2ec153df0c2c38fc4c197
Brandon Hoffmann <hoffm670@gmail.com>
2025-01-16 22:18:31 -0600

    updated re-deploy

--- redeploy.sh
+++ redeploy.sh
@@ -1,4 +1,6 @@
 cd <repo_path>
 git pull
 docker build -f Dockerfile.pi . -t mplus-api
+docker stop $(docker ps -a | grep mplus-api | awk '{print $1}')
+docker rm $(docker ps -a | grep mplus-api | awk '{print $1}')
 docker run -d -p 8080:8080 mplus-api
